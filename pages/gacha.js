0ff4fa2394c64817885f21dd273f823124dd4cfd
s21012 <s21012@std.it-college.ac.jp>
2024-08-01 12:29:22 +0900

    カレンダー機能を追加して、Todoの期限をきめれるようにした

--- pages/gacha.js
+++ pages/gacha.js
@@ -1,15 +1,21 @@
+import Head from 'next/head';
 import Gacha from '../components/Gacha';
 import Link from 'next/link';
 
 export default function GachaPage() {
   return (
-    <div style={{ textAlign: 'center', padding: '20px' }}>
-      <h1>ガチャシミュレーター</h1>
-      <Gacha />
-      <br />
-      <Link href="/">
-        ホームに戻る
-      </Link>
+    <div>
+      <Head>
+        <title>ガチャシミュレーター</title>
+        <meta name="description" content="ガチャシミュレーター画面" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main style={{ textAlign: 'center', marginTop: '50px' }}>
+        <Gacha />
+        <Link href="/">
+          <button>ホームに戻る</button>
+        </Link>
+      </main>
     </div>
   );
 }
--- pages/todo.js
+++ pages/todo.js
@@ -1,15 +1,21 @@
+import Head from 'next/head';
 import Todo from '../components/Todo';
 import Link from 'next/link';
 
 export default function TodoPage() {
   return (
-    <div style={{ textAlign: 'center', padding: '20px' }}>
-      <h1>TODOアプリ</h1>
-      <Todo />
-      <br />
-      <Link href="/">
-        ホームに戻る
-      </Link>
+    <div>
+      <Head>
+        <title>TODOリスト</title>
+        <meta name="description" content="TODOリスト画面" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main style={{ textAlign: 'center', marginTop: '50px' }}>
+        <Todo />
+        <Link href="/">
+          <button>ホームに戻る</button>
+        </Link>
+      </main>
     </div>
   );
 }
